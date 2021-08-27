9230c9b0d668b9708214ae7570e95511ee67b663
mohadesehazari98 <84409349+mohadesehazari98@users.noreply.github.com>
2021-08-28 00:22:36 +0430

    updated 28

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
             alphaArea: '.6',
         },
         scaleK: {
-            labels: ['HTML & CSS', 'JavaScript', 'Java', 'Laravel', 'CodeIgniter', 'MySQL', 'PHP'],
+            labels: ['Python', 'Matlab', 'Arduino', 'Html', 'Verilog', 'LaTeX', 'Qiskit'],
             item: {
                 fontColor: white,
                 rules: [{
